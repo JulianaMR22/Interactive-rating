b9a864c62e1ee920b9a63bfe8ad90ac1ba03c65c
JulianaMR22 <julianam2003@gmail.com>
2023-12-21 12:08:07 -0500

    Se mejoro el codigo JS

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 let mainSection = document.getElementById("mainSection");
 let secondSection = document.getElementById("secondSection");
 let btn__submit = document.getElementById("btn__submit");
-let btnOne = document.getElementById("btnOne");
-let btnTwo = document.getElementById("btnTwo");
-let btnThree = document.getElementById("btnThree");
-let btnFour = document.getElementById("btnFour");
-let btnFive = document.getElementById("btnFive");
 let punctuation = document.getElementById("punctuation");
-let calificacionBaja = parseInt(btnOne.textContent);
-let calificacionMediobaja = parseInt(btnTwo.textContent);
-let calificacionRegular = parseInt(btnThree.textContent);
-let calificacionBuena = parseInt(btnFour.textContent);
-let calificacionExcelente = parseInt(btnFive.textContent);
+const buttons = document.querySelectorAll(
+  ".main__container__circles .main__section__circle"
+);
+let calificacionBaja = parseInt(buttons[0].textContent);
+let calificacionMediobaja = parseInt(buttons[1].textContent);
+let calificacionRegular = parseInt(buttons[2].textContent);
+let calificacionBuena = parseInt(buttons[3].textContent);
+let calificacionExcelente = parseInt(buttons[4].textContent);
 
 btn__submit.addEventListener("click", () => {
   mainSection.classList.add("subsection__hidden");
@@ -20,23 +18,42 @@ btn__submit.addEventListener("click", () => {
   secondSection.classList.remove("subsection__hidden");
 });
 
-btnOne.addEventListener("click", () => {
-  btnOne.style.backgroundColor = "hsl(25, 97%, 53%)";
+buttons[0].addEventListener("click", () => {
+  buttons.forEach((button) => {
+    button.classList.remove("active__states");
+  });
+  buttons[0].classList.add("active__states");
+  // btnTwo.classList.remove("active__states");
+  // btnThree.classList.remove("active__states");
+  // btnFour.classList.remove("active__states");
+  // btnFive.classList.remove("active__states");
   punctuation.textContent = `you selected ${calificacionBaja} out of 5`;
 });
-btnTwo.addEventListener("click", () => {
-  btnTwo.style.backgroundColor = "hsl(25, 97%, 53%)";
+buttons[1].addEventListener("click", () => {
+  buttons.forEach((button) => {
+    button.classList.remove("active__states");
+  });
+  buttons[1].classList.add("active__states");
   punctuation.textContent = `you selected ${calificacionMediobaja} out of 5`;
 });
-btnThree.addEventListener("click", () => {
-  btnThree.style.backgroundColor = "hsl(25, 97%, 53%)";
+buttons[2].addEventListener("click", () => {
+  buttons.forEach((button) => {
+    button.classList.remove("active__states");
+  });
+  buttons[2].classList.add("active__states");
   punctuation.textContent = `you selected ${calificacionRegular} out of 5`;
 });
-btnFour.addEventListener("click", () => {
-  btnFour.style.backgroundColor = "hsl(25, 97%, 53%)";
+buttons[3].addEventListener("click", () => {
+  buttons.forEach((button) => {
+    button.classList.remove("active__states");
+  });
+  buttons[3].classList.add("active__states");
   punctuation.textContent = `you selected ${calificacionBuena} out of 5`;
 });
-btnFive.addEventListener("click", () => {
-  btnFive.style.backgroundColor = "hsl(25, 97%, 53%)";
+buttons[4].addEventListener("click", () => {
+  buttons.forEach((button) => {
+    button.classList.remove("active__states");
+  });
+  buttons[4].classList.add("active__states");
   punctuation.textContent = `you selected ${calificacionExcelente} out of 5`;
 });
